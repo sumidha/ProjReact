f966f7877fc7a15572a8bd4431308a3576e7c84f
shrutisinhaa <shruti.sinha@hotmail.com>
2021-05-26 11:28:47 +0530

    demo

--- src/Router/About.js
+++ src/Router/About.js
@@ -8,7 +8,7 @@ class About extends Component {
     render() { 
         return (
             
-            <h1>Demo of React App CI/CD</h1>
+            <h1>Demo of React App CI/CD for Future Developer</h1>
          );
     }
 }
